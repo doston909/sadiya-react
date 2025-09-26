02e82ff49d4d6b1eb178453822957599142f9de8
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-09-26 23:40:08 +0900

    feat: DAVE/create routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import {Container, Stack, Box, Typography, Button} from '@mui/material';
-import { RippleBadge } from './app/MaterialTheme/styled';
-
+import React from "react";
+import { Container, Stack, Box, Typography, Button } from "@mui/material";
+import { Link, Route, Switch } from "react-router-dom";
+import { About } from "./app/screen/About";
+import { Users } from "./app/MaterialTheme/Users";
 
 function App() {
-  return <Container sx={{ background: "orange"}}>
-    <Stack flexDirection={"column"}>
-      <Box sx={{my: 4}}>
-        <Typography variant="h4" component={"h4"}>
-          Hammaga salomlar
-        </Typography>
-      </Box>
-     <Box>
-        <RippleBadge badgeContent={5}>
-           <Button variant="contained">Contained</Button>
-        </RippleBadge>
-     </Box>
-    </Stack>
-  </Container> 
+  return (
+    <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+  );
+}
+
+function Home() {
+  return <Container>Home</Container>;
 }
 
 export default App;
