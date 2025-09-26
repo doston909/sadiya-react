437bdc07850f3a9238b01b6304336b25f9c2759a
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-09-26 23:07:34 +0900

    fix: DAVE/MUI customized integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import {Container, Stack, Box, Typography, Button} from '@mui/material';
+import { RippleBadge } from './app/MaterialTheme/styled';
 
 
 function App() {
-  return <Container maxWidth="sm">
+  return <Container sx={{ background: "orange"}}>
     <Stack flexDirection={"column"}>
       <Box sx={{my: 4}}>
         <Typography variant="h4" component={"h4"}>
           Hammaga salomlar
         </Typography>
       </Box>
-      <Button variant="contained">Contained</Button>
+     <Box>
+        <RippleBadge badgeContent={5}>
+           <Button variant="contained">Contained</Button>
+        </RippleBadge>
+     </Box>
     </Stack>
   </Container> 
 }
