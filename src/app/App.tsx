4d064e4b67b7aceb5e8becbbf4b0471daa4567bd
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-10-03 21:46:28 +0900

    integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../css/app.css';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { HelpPage } from './screens/helpPage';
-import { HomeNavbar } from './components/headers/HomeNavbar';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import { Footer } from './components/footer';
+
+import { Route, Switch, useLocation } from "react-router-dom";
+import HomePage from './screens/homePage';
+import ProductsPage from './screens/productsPage';
+import OrdersPage from './screens/ordersPage';
+import UserPage from './screens/userPage';
+import HelpPage from './screens/helpPage';
+import HomeNavbar from './components/headers/HomeNavbar';
+import OtherNavbar from './components/headers/OtherNavbar';
+import Footer from './components/footer';
 import '../css/app.css';
 import "../css/navbar.css"
 
--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import Basket from "./Basket";
 
-export function OtherNavbar() {
+export default function OtherNavbar() {
     const authMember = null;
     return (
         <div className="other-navbar">
@@ -9,7 +10,7 @@ export function OtherNavbar() {
         <Stack className="menu">
          <Box>
                 <NavLink to="/">
-                <img className="brand-logo" src="/icons/burak.svg"/>
+                <img className="brand-logo" src="/icons/sadiya.jpeg"/>
                 </NavLink>
          </Box>
          <Stack className="links">
@@ -42,7 +43,8 @@ export function OtherNavbar() {
                 Help
                 </NavLink>
          </Box>
-         {/* BASKET */}
+         
+         <Basket />
 
          {!authMember ? (
             <Box>
