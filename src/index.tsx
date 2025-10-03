91041b20b12dddd0c9af738a559dd15a5c0abc3f
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-10-03 14:53:01 +0900

    fix: organize header and footer components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeProvider} from "@mui/material/styles";
@@ -22,7 +22,6 @@ root.render(
           <App />
         </Router>
       </ThemeProvider>
-     
     </Provider>
   </React.StrictMode>
 );
